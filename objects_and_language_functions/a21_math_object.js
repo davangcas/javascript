e2d056b27616054d16cab9121817067ab22747f9
Angel David Castro <davangcas@gmail.com>
2023-11-22 17:27:22 -0300

    and and or operators

---
+++ objects_and_language_functions/a21_math_object.js
@@ -0,0 +1,34 @@
+// Example 1: Math Constants
+console.log("Math.PI:", Math.PI); // Prints the value of PI (3.141592653589793)
+console.log("Math.E:", Math.E); // Prints the value of Euler's number (2.718281828459045)
+console.log("Math.SQRT2:", Math.SQRT2); // Prints the square root of 2 (1.4142135623730951)
+
+// Example 2: Math Methods
+console.log("Math.abs(-5):", Math.abs(-5)); // Returns the absolute value of -5 (5)
+console.log("Math.ceil(4.2):", Math.ceil(4.2)); // Returns the smallest integer greater than or equal to 4.2 (5)
+console.log("Math.floor(4.8):", Math.floor(4.8)); // Returns the largest integer less than or equal to 4.8 (4)
+console.log("Math.round(4.5):", Math.round(4.5)); // Returns the value rounded to the nearest integer (5)
+console.log("Math.max(2, 5, 1, 10):", Math.max(2, 5, 1, 10)); // Returns the largest number from the arguments (10)
+console.log("Math.min(2, 5, 1, 10):", Math.min(2, 5, 1, 10)); // Returns the smallest number from the arguments (1)
+console.log("Math.pow(2, 3):", Math.pow(2, 3)); // Returns the value of 2 raised to the power of 3 (8)
+console.log("Math.sqrt(16):", Math.sqrt(16)); // Returns the square root of 16 (4)
+console.log("Math.random():", Math.random()); // Returns a random number between 0 (inclusive) and 1 (exclusive)
+
+// Example 3: Math Trigonometry
+console.log("Math.sin(Math.PI / 2):", Math.sin(Math.PI / 2)); // Returns the sine of PI/2 (1)
+console.log("Math.cos(Math.PI):", Math.cos(Math.PI)); // Returns the cosine of PI (-1)
+console.log("Math.tan(Math.PI / 4):", Math.tan(Math.PI / 4)); // Returns the tangent of PI/4 (1)
+
+// Example 4: Math Rounding
+console.log("Math.round(4.49):", Math.round(4.49)); // Returns the value rounded to the nearest integer (4)
+console.log("Math.floor(4.49):", Math.floor(4.49)); // Returns the largest integer less than or equal to 4.49 (4)
+console.log("Math.ceil(4.49):", Math.ceil(4.49)); // Returns the smallest integer greater than or equal to 4.49 (5)
+
+// Example 5: Math Random
+console.log("Math.random():", Math.random()); // Returns a random number between 0 (inclusive) and 1 (exclusive)
+console.log("Math.floor(Math.random() * 10):", Math.floor(Math.random() * 10)); // Returns a random integer between 0 and 9
+
+// Example 6: Math Logarithms
+console.log("Math.log(Math.E):", Math.log(Math.E)); // Returns the natural logarithm of Euler's number (1)
+console.log("Math.log10(100):", Math.log10(100)); // Returns the base 10 logarithm of 100 (2)
+console.log("Math.log2(8):", Math.log2(8)); // Returns the base 2 logarithm of 8 (3)
---
+++ objects_and_language_functions/a22_short_circuit_operator.js
@@ -0,0 +1,31 @@
+// OR operator
+
+let x = 5;
+let y = 10;
+
+let result = x || y;
+
+console.log(result);
+
+let name = "";
+let defaultName = "John Doe";
+
+result = name || defaultName;
+
+console.log(result);
+
+// AND operator
+
+let a = 5;
+let b = 10;
+
+result = a && b;
+
+console.log(result);
+
+let name2 = "";
+let defaultName2 = "John Doe";
+
+result = name2 && defaultName2;
+
+console.log(result);
