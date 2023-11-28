903a195bddb7bd03940b33da67f870159f8936c5
Angel David Castro <davangcas@gmail.com>
2023-11-28 16:32:28 -0300

    responsive tester

---
+++ dom_exercises/a08.js
@@ -0,0 +1,22 @@
+let tester;
+
+export function responsiveTester(form_id) {
+    const form = document.getElementById(form_id);
+
+    document.addEventListener("submit", (event) => {
+        if (event.target === form) {
+            event.preventDefault();
+            tester = window.open(
+                form.url.value,
+                "tester",
+                `innerWidth=${form.width.value}, innerHeight=${form.height.value}`
+            );
+        }
+    });
+
+    document.addEventListener("click", (event) => {
+        if (event.target === form.close) {
+            tester.close();
+        }
+    });
+}
--- dom_exercises/index.js
+++ dom_exercises/index.js
@@ -5,6 +5,7 @@ import { countdown } from "./a04.js";
 import { scrollTop } from "./a05.js";
 import { darkModeToogle } from "./a06.js";
 import { responsiveMedia } from "./a07.js";
+import { responsiveTester } from "./a08.js";
 
 document.addEventListener("DOMContentLoaded", (event) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
@@ -24,6 +25,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         `<a href="https://goo.gl/maps/2QzQF9VhVv5Zb5Lr9" target="_blank" rel="noopener">Ver mapa</a>`,
         `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.059666095283!2d-99.1675888850951!3d19.42899338688558!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xb85dd2b7b4c6f3d%3A0x2e1c9b3f6f7a3b9b!2sCalle%20de%20Londres%20105%2C%20Ju%C3%A1rez%2C%20Cuauht%C3%A9moc%2C%2006600%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e0!3m2!1ses-419!2smx!4v1614576661001!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
     );
+    responsiveTester("responsive-tester");
 });
 
 document.addEventListener("keydown", (event) => {
